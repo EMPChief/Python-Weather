0398346ed2cd8ff2a3eb48439b198908a9d1308b
empchief <bjornmagnekristensen1999@gmail.com>
2024-01-28 19:55:48 +0100

    Refactor code and add dictionary endpoint

---
+++ convert.py
@@ -0,0 +1,42 @@
+import xmltodict
+import json
+import re
+
+def convert_entry(entry):
+    # Remove patterns like "#n#6" from the "word" field
+    word_without_pattern = re.sub(r'#\w+#\d+', '', entry["@word"])
+    
+    return {
+        "word": word_without_pattern,
+        "status": entry["@status"],
+        "gloss": entry["gloss"],
+        "lf": entry["lf"]
+    }
+
+def convert_xml_to_json(xml_string):
+    dict_data = xmltodict.parse(xml_string)
+    entries = dict_data.get("wne", {}).get("entry", [])
+    converted_entries = [convert_entry(entry) for entry in entries] if isinstance(entries, list) else [convert_entry(entries)]
+    result_dict = {
+        "entries": converted_entries
+    }
+
+    json_data = json.dumps(result_dict, indent=2)
+    return json_data
+
+def read_xml_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        xml_string = file.read()
+    return xml_string
+
+def save_json_file(json_data, output_file_path):
+    with open(output_file_path, 'w', encoding='utf-8') as json_file:
+        json_file.write(json_data)
+
+if __name__ == "__main__":
+    xml_file_path = "wne-2006-12-06.xml"
+    output_json_file_path = "dictionary.json"
+
+    xml_string = read_xml_file(xml_file_path)
+    json_data = convert_xml_to_json(xml_string)
+    save_json_file(json_data, output_json_file_path)
--- main.py
+++ main.py
@@ -1,23 +1,38 @@
 from flask import Flask, render_template, request, jsonify
-import pandas as pd
 import json
+
 class WeatherAPP:
     def __init__(self):
         self.app = Flask('WeatherAPP')
         self.load_data()
-    
+
     def load_data(self):
         with open('data.json', 'r') as f:
             self.weather_data = json.load(f)
-    
+        self.load_dictionary_data()
+
+    def load_dictionary_data(self):
+        with open('dictionary.json', 'r') as f:
+            self.dictionary_data = json.load(f)
+
     def run(self):
         self.app.run(host='0.0.0.0', port=3000, debug=True)
-    
+
     def routes(self):
         @self.app.route('/')
         def index():
             return render_template('index.html')
-
+        
+        @self.app.route("/dictionary/<word>")
+        def get_dictionary(word):
+            result = {
+                'error': 'Word not found in database!'
+            }
+            for entry in self.dictionary_data.get("entries", []):
+                if entry.get("word") == word:
+                    result = entry
+                    break
+            return jsonify(result)
 
         @self.app.route("/weather/<station>/<date>")
         def get_weather(station, date):
@@ -26,15 +41,9 @@ class WeatherAPP:
             }
             for entry in self.weather_data.get("weatherData", []):
                 if entry.get("station") == station and entry["date"] == date:
-
                     result = entry
                     break
             return jsonify(result)
-
-
-
-
-
 
 if __name__ == '__main__':
     weather_app = WeatherAPP()
