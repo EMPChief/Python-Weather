47e54037e6bd1fc3b369086b2a6fa185f60f569c
empchief <bjornmagnekristensen1999@gmail.com>
2024-02-01 19:34:37 +0100

    Update happy.py and main_streamlit.py, and requirements.txt

--- main_streamlit.py
+++ main_streamlit.py
@@ -12,7 +12,7 @@ st.image(logo_path, width=100, output_format="PNG")
 st.markdown("<h1 style='text-align: center; color: #1f4e79;'>Weather Forecast</h1>",
             unsafe_allow_html=True)
 
-city_name = st.text_input("Enter a city name:", "Bergen")
+city_name = st.text_input("Enter a city name:", "Lurøy, NO")
 forecast_days = st.slider("How many days in advance do you want to see the weather forecast?",
                           min_value=1, max_value=5,
                           help="Select the number of forecasted days.")
